3e71448e05702e5591af5148df516d7ec5021ec6
Justin Lin <deeznuts@Justins-MacBook-Air.local>
2022-01-05 16:19:00 -0800

    <items/models.py>

--- items/models.py
+++ items/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Date(models.Model):
+    date = models.DateField()
+
+
+class Transactions(models.Model):
+    totalcost = models.DecimalField(max_digits=100, decimal_places=2)
+    time_occured = models.ForeignKey(Date, on_delate=models.CASCADE)
+
+
+class Items(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.DecimalField(max_digits=100, decimal_places=2)
+    column = models.CharField(max_length=3)
+    max_quota = models.IntegerField()
+    purchased_items = models.ForeignKey(Transactions, on_delete=models.CASCADE)
+
+
+
+
