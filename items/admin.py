aa4d60b57def5b56584bdd9a760a76bb06d46db9
Justin Lin <deeznuts@Justins-MacBook-Air.local>
2022-01-05 16:46:30 -0800

    fix the bugs

--- items/admin.py
+++ items/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Date, Transaction, Item
 
 # Register your models here.
+
+admin.site.register(Date)
+admin.site.register(Transaction)
+admin.site.register(Item)
--- items/models.py
+++ items/models.py
@@ -3,19 +3,20 @@ from django.db import models
 # Create your models here.
 class Date(models.Model):
     date = models.DateField()
+    spreadsheet_row = models.IntegerField()
 
 
-class Transactions(models.Model):
+class Transaction(models.Model):
     totalcost = models.DecimalField(max_digits=100, decimal_places=2)
-    time_occured = models.ForeignKey(Date, on_delate=models.CASCADE)
+    time_occured = models.ForeignKey(Date, on_delete=models.CASCADE)
 
 
-class Items(models.Model):
+class Item(models.Model):
     name = models.CharField(max_length=30)
     price = models.DecimalField(max_digits=100, decimal_places=2)
     column = models.CharField(max_length=3)
     max_quota = models.IntegerField()
-    purchased_items = models.ForeignKey(Transactions, on_delete=models.CASCADE)
+    purchased_items = models.ForeignKey(Transaction, on_delete=models.CASCADE)
 
 
 
